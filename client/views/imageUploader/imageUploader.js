8b8ae090882a3959f4fb4cdb6dfd8f37676d27d8
Wong Kin Chun <wongkinchun@Kins-iMac.local>
2015-08-24 01:30:06 +0800

    added crop image

--- client/views/imageUploader/imageUploader.js
+++ client/views/imageUploader/imageUploader.js
@@ -1,15 +1,65 @@
+var imageEditor = '';
+
+Template.imageUploader.helpers({
+	getURL: function(){ return this.url(); },
+	allImages: function(){ return Images.find(); },
+	imageUploaded: function(){
+		return (!!Session.get('previewImageSrc')) ? '' : 'hidden';
+	},
+	previewImageSrc: function(){
+		var imageID = Session.get('previewImageSrc');
+		var url = (imageID) ? Images.files.findOne(imageID).url() : '';
+		if(url != ''){
+			imageEditor.setImage(url);
+		}
+		return url;
+	}
+});
 Template.imageUploader.events({
-	
-	'change .imageInput': function(event, template){
+	'click #saveImageSize': function(){
+		var imageID = Session.get('previewImageSrc');
+		var cropArea = imageEditor.tellSelect();
+		Meteor.call('cropImage', imageID, cropArea, function(err, result){
+			if(err) {
+				Bert.alert(err.reason, 'danger', 'growl-top-right');
+			} else if(!result){
+				Bert.alert('Please select an area', 'danger', 'growl-top-right');
+			} else {
+				Session.set('previewImageSrc', '');
+				Bert.alert('Image cropped', 'success', 'growl-top-right');
+			}
+			$('#fileUploadForm')[0].reset();
+		});
+		return false;
+	},
+	'change #editImage': function(event, template){
 		FS.Utility.eachFile(event, function(file) {
 			Images.insert(file, function (err, fileObj) {
 				if (err){
 					Bert.alert(err.reason, 'danger', 'growl-top-right');
 				} else {
-					console.log(fileObj);
+					Session.set('previewImageSrc', fileObj._id);
 				}
 			});
 		});
 	}
-
+});
+Template.imageUploader.onRendered(function(){
+	imageEditor = $('#previewImage').Jcrop({ boxWidth: 500, boxHeight: 300, aspectRatio: 1 }).data('Jcrop');
+	var medium = new MediumEditor('#demo', {
+        mode: MediumEditor.richMode,
+        attributes: null,
+        tags: null,
+        toolbar: false
+    });
+    medium.subscribe('editableInput', function (event, editable) {
+    	var $target = $(event.target);
+    	if ($target[0].scrollHeight > $target.innerHeight()) {
+			$target.css('border', '1px solid red');
+			$target.css('overflow-y', 'scroll');
+		} else {
+			$target.css('border', '1px dashed black');
+			$target.css('overflow', 'hidden');
+		}
+	});
 });
--- server/lib/methods.js
+++ server/lib/methods.js
@@ -7,5 +7,12 @@ Meteor.methods({
 	register: function(doc){
 		check(doc, Schemas.User);
 		return Accounts.createUser(doc);
+	},
+	cropImage: function(imageID, cp){
+		var readStream = Images.findOne(imageID).createReadStream('images');
+		var writeStream = Images.findOne(imageID).createWriteStream('images');
+		if(cp.w == 0 || cp.h == 0) return false;
+		gm(readStream).crop(cp.w, cp.h, cp.x, cp.y).stream().pipe(writeStream);
+		return imageID;
 	}
 });
